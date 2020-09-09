8ef15eba16e87c491ac67402b7b4ebefa57a0ca1
Andrea Kern <andi_lake14@hotmail.com>
2020-09-08 23:40:46 -0500

    Página creada con productos cargados, al agregar a la canasta ya se actualiza el número de artículos, pero el subtotal aún no

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Product from "./Product"
+import "./Home.css";
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w1500._CB406837584_.jpg" alt="Home"/>
+            
+                <div className="home__row">
+                    <Product id="0012" title="Apple iPad Mini (Wi-Fi, 64GB) - Space Gray" image="https://images-na.ssl-images-amazon.com/images/I/71Ha06XS-VL._AC_SL1500_.jpg" price={349.99} rating={5} />
+                    <Product id="0013" title="HyperX Cloud Flight - Wireless Gaming Headset" image="https://images-na.ssl-images-amazon.com/images/I/71ap89elAuL._AC_SL1428_.jpg" price={139.99} rating={4} />
+                    <Product id="0014" title="Final Fantasy XV Royal Edition - PlayStation 4" image="https://images-na.ssl-images-amazon.com/images/I/61ExXfg6NyL.jpg" price={19.58} rating={5} />
+                </div>
+
+                <div className="home__row">
+                    <Product id="0015" title="Sceptre 30-inch Curved Gaming Monitor 21:9 2560x1080 Ultra Wide" image="https://images-na.ssl-images-amazon.com/images/I/81Wt3h7-V2L._AC_SL1500_.jpg" price={278.99} rating={4} />
+                    <Product id="0016" title="DIERYA Mechanical Gaming Keyboard 60% True RGB Backlit Bluetooth" image="https://images-na.ssl-images-amazon.com/images/I/61zMq16ZDxL._AC_SL1200_.jpg" price={54.99} rating={4} />
+                </div>
+
+                <div className="home__row">
+                    <Product id="0017" title="Echo Dot (3rd Gen) with Clock with Mandalorian The Child stand" image="https://images-na.ssl-images-amazon.com/images/I/51ka-NCLsXL._AC_SL1000_.jpg" price={74.94} rating={4} />
+                    <Product id="0018" title="ASUS VivoBook 15 Thin and Light Laptop, 15.6” FHD" image="https://images-na.ssl-images-amazon.com/images/I/81mZ4eDWYkL._AC_SL1500_.jpg" price={551.00} rating={5} />
+                    <Product id="0019" title="Nintendo Switch Lite - Yellow" image="https://images-na.ssl-images-amazon.com/images/I/71q4BBqaNvL._AC_SL1500_.jpg" price={220.00} rating={5} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-hl3WKD3KBEaOGRIx5DfucKoauz0_lKs",
+    authDomain: "clone-c1075.firebaseapp.com",
+    databaseURL: "https://clone-c1075.firebaseio.com",
+    projectId: "clone-c1075",
+    storageBucket: "clone-c1075.appspot.com",
+    messagingSenderId: "362420306740",
+    appId: "1:362420306740:web:e72c88605300b1bc1f9b61",
+    measurementId: "G-W48FFDDK01"
+  };
