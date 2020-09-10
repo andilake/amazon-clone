65708cd4af54872b0a6072871813f38581430f4b
Andrea Kern <andi_lake14@hotmail.com>
2020-09-09 22:22:53 -0500

    Cambios esteticos.

--- src/Checkout.js
+++ src/Checkout.js
@@ -3,6 +3,8 @@ import "./Checkout.css";
 import Subtotal from './Subtotal';
 import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
 
 function Checkout() {
     const[{ basket, user }, dispatch] =useStateValue();
@@ -13,19 +15,31 @@ function Checkout() {
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
                 
                 <div>
-                    <h3>Hello, {user?.email}</h3>
-                    <h2 className="checkout__title">Your shopping basket</h2>
+                    <h3>{user ? 'Hi ' + user?.email + '!' : 'Hello Guest'}</h3>
+                    <h2 className="checkout__title">Shopping Cart</h2>
                 
-
-                {basket.map(item => (
-                    <CheckoutProduct 
-                        id={item.id}
-                        title={item.title}
-                        image={item.image}
-                        price={item.price}
-                        rating={item.rating}
-                    />
-                ))}
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
+                
+                
+                <CurrencyFormat renderText={(value) => (
+                    <>
+                        <p className="checkout__subtotal">{basket?.length==0 ? "Your shopping cart is empty" : 'Subtotal (' + basket?.length + ' items): ' + value}</p>
+                    </>
+                    )}
+                    decimalScale={2}
+                    value={getBasketTotal(basket)}
+                    displayType={"text"}
+                    thousandSeparator={true}
+                    prefix={"$"}
+                />
                 
 
                 </div>
--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -19,11 +19,13 @@ function CheckoutProduct({ id, image, title, price, rating}) {
             <img className='checkoutProduct__image' src={image} alt=""/>
 
             <div className='checkoutProduct__info'>
-                <p className='checkoutProduct__title'>{title}</p>
-                <p className='checkoutProduct__price'>
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
+                <div className='checkoutProduct_info_title'>
+                    <p className='checkoutProduct__title'>{title}</p>
+                    <p className='checkoutProduct__price'>
+                        <small>$</small>
+                        <strong>{price}</strong>
+                    </p>
+                </div>
                 <div className='checkoutProduct__rating'>
                     {Array(rating)
                         .fill()
